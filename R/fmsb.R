71cdc38908b0129a55261cac2a72e2aaef155281
Minato Nakazawa <minatonakazawa@gmail.com>
2024-01-19 10:10:02 +0000

    version 0.7.6

--- R/fmsb.R
+++ R/fmsb.R
@@ -24,6 +24,9 @@
 #           IRCIPois(), updated pvalueplot(), fixed wrong message in riskdifference().
 # rev. 0.7.3, 1 March 2020, spearman.ci.sas(), truemedian() and geary.test() now 
 #           can properly treat data with missing values.
+# rev. 0.7.6, 16 January 2024, radarchartcirc(), RDMH(), RRMH(), IRDMH(), and 
+#           IRRMH() were added. radarchartcirc() is a circular radar grid
+#           version of radarchart().
 
 SIQR <- function(X, mode=1) { 
  if (mode==1) { ret <- (fivenum(X)[4]-fivenum(X)[2])/2 }
@@ -460,6 +463,113 @@ radarchart <- function(df, axistype=0, seg=4, pty=16, pcol=1:8, plty=1:6, plwd=1
   }
 }
 
+radarchartcirc <- function(df, axistype=0, seg=4, pty=16, pcol=1:8, plty=1:6, plwd=1,
+                       pdensity=NULL, pangle=45, pfcol=NA, cglty=1, cglwd=1,
+                       cglcol="navy", axislabcol="blue", title="", maxmin=TRUE,
+                       na.itp=TRUE, centerzero=FALSE, vlabels=NULL, vlcex=NULL,
+                       caxislabels=NULL, calcex=NULL,
+                       paxislabels=NULL, palcex=NULL, ...) {
+  if (!is.data.frame(df)) { cat("The data must be given as dataframe.\n"); return() }
+  if ((n <- length(df))<3) { cat("The number of variables must be 3 or more.\n"); return() }
+  if (maxmin==FALSE) { # when the dataframe does not include max and min as the top 2 rows.
+    dfmax <- apply(df, 2, max)
+    dfmin <- apply(df, 2, min)
+    df <- rbind(dfmax, dfmin, df)
+  }
+  plot(c(-1.2, 1.2), c(-1.2, 1.2), type="n", frame.plot=FALSE, axes=FALSE, 
+       xlab="", ylab="", main=title, asp=1, ...) # define x-y coordinates without any plot
+  theta <- seq(90, 450, length=n+1)*pi/180
+  theta <- theta[1:n]
+  xx <- cos(theta)
+  yy <- sin(theta)
+  theta2 <- (90+0:120*3)*pi/180
+  xxx <- cos(theta2)
+  yyy <- sin(theta2)
+  CGap <- ifelse(centerzero, 0, 1)
+  for (i in 0:seg) { # complementary guide lines, dotted navy line by default
+    polygon(xxx*(i+CGap)/(seg+CGap), yyy*(i+CGap)/(seg+CGap), lty=cglty, lwd=cglwd, border=cglcol)
+    if (axistype==1|axistype==3) CAXISLABELS <- paste(i/seg*100,"(%)")
+    if (axistype==4|axistype==5) CAXISLABELS <- sprintf("%3.2f",i/seg)
+    if (!is.null(caxislabels)&(i<length(caxislabels))) CAXISLABELS <- caxislabels[i+1]
+    if (axistype==1|axistype==3|axistype==4|axistype==5) {
+     if (is.null(calcex)) text(-0.05, (i+CGap)/(seg+CGap), CAXISLABELS, col=axislabcol) else
+     text(-0.05, (i+CGap)/(seg+CGap), CAXISLABELS, col=axislabcol, cex=calcex)
+    }
+  }
+  if (centerzero) {
+    arrows(0, 0, xx*1, yy*1, lwd=cglwd, lty=cglty, length=0, col=cglcol)
+  }
+  else {
+    arrows(xx/(seg+CGap), yy/(seg+CGap), xx*1, yy*1, lwd=cglwd, lty=cglty, length=0, col=cglcol)
+  }
+  PAXISLABELS <- df[1,1:n]
+  if (!is.null(paxislabels)) PAXISLABELS <- paxislabels
+  if (axistype==2|axistype==3|axistype==5) {
+   if (is.null(palcex)) text(xx[1:n], yy[1:n], PAXISLABELS, col=axislabcol) else
+   text(xx[1:n], yy[1:n], PAXISLABELS, col=axislabcol, cex=palcex)
+  }
+  VLABELS <- colnames(df)
+  if (!is.null(vlabels)) VLABELS <- vlabels
+  if (is.null(vlcex)) text(xx*1.2, yy*1.2, VLABELS) else
+  text(xx*1.2, yy*1.2, VLABELS, cex=vlcex)
+  series <- length(df[[1]])
+  SX <- series-2
+  if (length(pty) < SX) { ptys <- rep(pty, SX) } else { ptys <- pty }
+  if (length(pcol) < SX) { pcols <- rep(pcol, SX) } else { pcols <- pcol }
+  if (length(plty) < SX) { pltys <- rep(plty, SX) } else { pltys <- plty }
+  if (length(plwd) < SX) { plwds <- rep(plwd, SX) } else { plwds <- plwd }
+  if (length(pdensity) < SX) { pdensities <- rep(pdensity, SX) } else { pdensities <- pdensity }
+  if (length(pangle) < SX) { pangles <- rep(pangle, SX)} else { pangles <- pangle }
+  if (length(pfcol) < SX) { pfcols <- rep(pfcol, SX) } else { pfcols <- pfcol }
+  for (i in 3:series) {
+    xxs <- xx
+    yys <- yy
+    scale <- CGap/(seg+CGap)+(df[i,]-df[2,])/(df[1,]-df[2,])*seg/(seg+CGap)
+    if (sum(!is.na(df[i,]))<3) { cat(sprintf("[DATA NOT ENOUGH] at %d\n%g\n",i,df[i,])) # for too many NA's (1.2.2012)
+    } else {
+      for (j in 1:n) {
+        if (is.na(df[i, j])) { # how to treat NA
+          if (na.itp) { # treat NA using interpolation
+            left <- ifelse(j>1, j-1, n)
+            while (is.na(df[i, left])) {
+              left <- ifelse(left>1, left-1, n)
+            }
+            right <- ifelse(j<n, j+1, 1)
+            while (is.na(df[i, right])) {
+              right <- ifelse(right<n, right+1, 1)
+            }
+            xxleft <- xx[left]*CGap/(seg+CGap)+xx[left]*(df[i,left]-df[2,left])/(df[1,left]-df[2,left])*seg/(seg+CGap)
+            yyleft <- yy[left]*CGap/(seg+CGap)+yy[left]*(df[i,left]-df[2,left])/(df[1,left]-df[2,left])*seg/(seg+CGap)
+            xxright <- xx[right]*CGap/(seg+CGap)+xx[right]*(df[i,right]-df[2,right])/(df[1,right]-df[2,right])*seg/(seg+CGap)
+            yyright <- yy[right]*CGap/(seg+CGap)+yy[right]*(df[i,right]-df[2,right])/(df[1,right]-df[2,right])*seg/(seg+CGap)
+            if (xxleft > xxright) {
+              xxtmp <- xxleft; yytmp <- yyleft;
+              xxleft <- xxright; yyleft <- yyright;
+              xxright <- xxtmp; yyright <- yytmp;
+            }
+            xxs[j] <- xx[j]*(yyleft*xxright-yyright*xxleft)/(yy[j]*(xxright-xxleft)-xx[j]*(yyright-yyleft))
+            yys[j] <- (yy[j]/xx[j])*xxs[j]
+          } else { # treat NA as zero (origin)
+            xxs[j] <- 0
+            yys[j] <- 0
+          }
+        }
+        else {
+          xxs[j] <- xx[j]*CGap/(seg+CGap)+xx[j]*(df[i, j]-df[2, j])/(df[1, j]-df[2, j])*seg/(seg+CGap)
+          yys[j] <- yy[j]*CGap/(seg+CGap)+yy[j]*(df[i, j]-df[2, j])/(df[1, j]-df[2, j])*seg/(seg+CGap)
+        }
+      }
+      if (is.null(pdensities)) {
+        polygon(xxs, yys, lty=pltys[i-2], lwd=plwds[i-2], border=pcols[i-2], col=pfcols[i-2])
+      } else {
+        polygon(xxs, yys, lty=pltys[i-2], lwd=plwds[i-2], border=pcols[i-2], 
+         density=pdensities[i-2], angle=pangles[i-2], col=pfcols[i-2])
+      }
+      points(xx*scale, yy*scale, pch=ptys[i-2], col=pcols[i-2])
+    }
+  }
+}
+
 pvalueplot <- function(XTAB, plot.OR=FALSE, plot.log=FALSE, xrange=c(0.01,5), add=FALSE, ...) {
 # XTAB must be 2x2 cross table.
 # ref. Rothman KJ (2002) Epidemiology: An introduction. Oxford Univ. Press
@@ -636,5 +746,82 @@ IRCIPois <- function(a, PT, conf.level=0.9) {
  aL <- qchisq((1-conf.level)/2, a*2)/2
  aU <- qchisq(1-(1-conf.level)/2, (a+1)*2)/2
  return(list(IR=IR, IRL=aL/PT, IRU=aU/PT))
+}
+
+# pooled Risk Difference (Mantel-Haenszel) with CI
+# Chapter 10 of Epidemiology: An Introduction (Rothman)
+RDMH <- function(XTAB, conf.level=0.9) { 
+# XTAB includes 4 columns as ai, bi, N1i, N0i, rows are ith strata
+ai <- XTAB[, 1]
+bi <- XTAB[, 2]
+N1i <- XTAB[, 3]
+N0i <- XTAB[, 4]
+Ti <- rowSums(XTAB[, 3:4])
+ci <- N1i - ai
+di <- N0i - bi
+rdmh <- sum((ai*N0i - bi*N1i)/Ti) / sum(N1i*N0i/Ti)
+numerator <- sum((N1i*N0i/Ti)^2 * (ai*ci/(N1i^2*(N1i-1)) + bi*di/(N0i^2*(N0i-1))))
+denominator <- (sum(N1i*N0i/Ti))^2
+varrdmh <- numerator/denominator
+rdmhll <- rdmh - qnorm(1-(1-conf.level)/2)*sqrt(varrdmh)
+rdmhul <- rdmh + qnorm(1-(1-conf.level)/2)*sqrt(varrdmh)
+return(list(estimate=rdmh, conf.int=c(rdmhll, rdmhul), conf.level=conf.level))
+}
+
+# pooled Risk Ratio (Mantel-Haenszel) with CI
+# Chapter 10 of Epidemiology: An Introduction (Rothman)
+RRMH <- function(XTAB, conf.level=0.9) { 
+# XTAB includes 4 columns as ai, bi, N1i, N0i, rows are ith strata
+ai <- XTAB[, 1]
+bi <- XTAB[, 2]
+N1i <- XTAB[, 3]
+N0i <- XTAB[, 4]
+Ti <- rowSums(XTAB[, 3:4])
+M1i <- ai + bi
+rrmh <- sum(ai*N0i/Ti) / sum(bi*N1i/Ti)
+numerator <- sum(M1i*N1i*N0i/Ti^2 - ai*bi/Ti)
+denominator <- sum(ai*N0i/Ti)*sum(bi*N1i/Ti)
+varlnrrmh <- numerator/denominator
+rrmhll <- exp(log(rrmh) - qnorm(1-(1-conf.level)/2)*sqrt(varlnrrmh))
+rrmhul <- exp(log(rrmh) + qnorm(1-(1-conf.level)/2)*sqrt(varlnrrmh))
+return(list(estimate=rrmh, conf.int=c(rrmhll, rrmhul), conf.level=conf.level))
+}
+
+# pooled Incidence Rate Difference (Mantel-Haenszel) with CI
+# Chapter 10 of Epidemiology: An Introduction (Rothman)
+IRDMH <- function(XTAB, conf.level=0.9) {
+# XTAB includes 4 columns as ai, bi, PT1i, PT0i, rows are ith data
+ai <- XTAB[, 1]
+bi <- XTAB[, 2]
+PT1i <- XTAB[, 3]
+PT0i <- XTAB[, 4]
+Mi <- rowSums(XTAB[, 1:2])
+Ti <- rowSums(XTAB[, 3:4])
+irdmh <- sum((ai*PT0i-bi*PT1i)/Ti)/sum(PT1i*PT0i/Ti)
+numerator <- sum((PT1i*PT0i/Ti)^2*(ai/PT1i^2+bi/PT0i^2))
+denominator <- sum(PT1i*PT0i/Ti)^2
+varirdmh <- numerator/denominator
+irdmhll <- irdmh - qnorm(1-(1-conf.level)/2)*sqrt(varirdmh)
+irdmhul <- irdmh + qnorm(1-(1-conf.level)/2)*sqrt(varirdmh)
+return(list(estimate=irdmh, conf.int=c(irdmhll, irdmhul), conf.level=conf.level))
+}
+
+# pooled Incidence Rate Ratio (Mantel-Haenszel) with CI
+# Chapter 10 of Epidemiology: An Introduction (Rothman)
+IRRMH <- function(XTAB, conf.level=0.9) {
+# XTAB includes 4 columns as ai, bi, PT1i, PT0i, rows are ith data
+ai <- XTAB[, 1]
+bi <- XTAB[, 2]
+PT1i <- XTAB[, 3]
+PT0i <- XTAB[, 4]
+Mi <- rowSums(XTAB[, 1:2])
+Ti <- rowSums(XTAB[, 3:4])
+irrmh <- sum(ai*PT0i/Ti)/sum(bi*PT1i/Ti)
+numerator <- sum(Mi*PT1i*PT0i/Ti^2)
+denominator <- sum(ai*PT0i/Ti)*sum(bi*PT1i/Ti)
+varlnirrmh <- numerator/denominator
+irrmhll <- exp(log(irrmh) - qnorm(1-(1-conf.level)/2)*sqrt(varlnirrmh))
+irrmhul <- exp(log(irrmh) + qnorm(1-(1-conf.level)/2)*sqrt(varlnirrmh))
+return(list(estimate=irrmh, conf.int=c(irrmhll, irrmhul), conf.level=conf.level))
 }
 
